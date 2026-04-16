0f91ac620b06df0832925a4784ecc30ab2cd89de
Goluboy <ksushaskorynina@gmail.com>
2026-04-16 19:25:48 +0500

    module1-task2

---
+++ src/types/offer.type.ts
@@ -0,0 +1,19 @@
+import { City } from "./City";
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: string;
+  city: City;
+  previewImage: string;
+  images: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  type: 'apartment' | 'house' | 'room' | 'hotel';
+  rooms: number;
+  guests: number;
+  price: number;
+  facilities: string[];
+  author: string;
+};
